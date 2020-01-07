9ef92a7a7cadcab197360b715546281a7de3e1ca
Karli Steeves <steeves.karli@gmail.com>
2020-01-07 12:10:29 -0600

    servlets servlets

--- src/main/java/PageCounterServlet.java
+++ src/main/java/PageCounterServlet.java
@@ -3,14 +3,16 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "PageCounterServlet", urlPatterns = "/count")
+public class PageCounterServlet extends HttpServlet {
+
+    int count = 0;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1>" + (count++) +"</h1>");
 
 
     }
